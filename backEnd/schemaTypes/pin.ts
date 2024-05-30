a81fda06fca7e6ce1043ea9f658001c648f1392d
Yijie Zou <yijiezou2004@gmail.com>
2024-05-30 17:50:20 -0600

    Setup base

---
+++ backEnd/schemaTypes/pin.ts
@@ -0,0 +1,57 @@
+export default{
+    name: 'pin',
+    title: "Pin",
+    type: "document",
+    fields: [
+        {
+            name: "title",
+            title: "Title",
+            type: "string"
+        },
+        {
+            name: "info",
+            title: "Info",
+            type: "string"
+        },
+        {
+            name: "destination",
+            title: "Destination",
+            type: "string"
+        },
+        {
+            name: "category",
+            title: "Category",
+            type: "string"
+        },
+        {
+            name: "image",
+            title: "Image",
+            type: "image",
+            options: {
+                hotspot: true
+            }
+        },
+        {
+            name: "userID",
+            title: "UserID",
+            type: "string"
+        },
+        {
+            name: "postedBY",
+            title: "PostedBy",
+            type: "postedBy"
+        },
+        {
+            name: "save",
+            title: "Save",
+            type: "array",
+            of: [{type: 'save'}]
+        },
+        {
+            name: "comments",
+            title: "Comments",
+            type: "array",
+            of: [{type: 'comment'}]
+        }
+    ]
+}
